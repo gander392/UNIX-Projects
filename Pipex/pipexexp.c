c62306d0641e9a7aecc5c537cc8b32cd319b72a5
gander392 <71662039+gander392@users.noreply.github.com>
2021-02-10 16:21:56 -0500

    Update pipexexp.c

--- Pipex/pipexexp.c
+++ Pipex/pipexexp.c
@@ -1,6 +1,6 @@
 //pipexExp.c
 
-#incliude <stdio.h>
+#include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
@@ -12,7 +12,7 @@ void pipeFunc(char **, int, int);
 
 void main(int ac, char **av) {
 	if(ac < 3) {
-		fprintf(stderr, 'usage: ./pipex cmd1 cmd2\n');
+		fprintf(stderr, "usage: ./pipex cmd1 cmd2\n");
 		exit(1);
 	}else
 		pipeFunc(av, 1, 0);
@@ -24,29 +24,29 @@ void pipeFunc(char **av, int i, int read) {
 			if(dup2(read, 0) != -1)
 				close(read); //stdin is successfully redirected
 			else
-				oops('could not redirect stdin for the last program entered\n', 1);
+				oops("could not redirect stdin for the last program entered\n", 1);
 		}
 		execlp(av[i], av[i], NULL);
-		oops('could not execute the last program entered\n', 1);
+		oops("could not execute the last program entered\n", 1);
 	}else
 		int thePipe[2]; //pid
 	
 		if(pipe(thePipe) == -1)
-			oops('could not pipe\n'), 1);
+			oops("could not pipe\n"), 1);
 
 		switch(fork()) {
 		case -1:
-			oops('could not fork\n', 1);
+			oops("could not fork\n", 1);
 			break;
 		case 0:
 			childProc = 1;
 			close(thePipe[0]); //break stdin
 			if(dup2(read, 0) == -1) //read from fdIn
-				oops('could not redirect stdin\n', 1);
+				oops("could not redirect stdin\n", 1);
 			if(dup2(thePipe[1], 1) == -1) // write to thePipe[1]
-				oops('could not redirect stdout\n', 1);
+				oops("could not redirect stdout\n", 1);
 			execlp(av[i], av[i], NULL);
-			oops('could not execute\n', 1);
+			oops("could not execute\n", 1);
 			break;
 		default:
 			close(thePipe[1]); //parent process executes the remaining programs
