cc2acf5a258072c4630a33e8cdf0e10651edd645
gander392 <71662039+gander392@users.noreply.github.com>
2021-02-09 20:00:13 -0500

    Update paddle.h

--- Pong/paddle.h
+++ Pong/paddle.h
@@ -1,18 +1,10 @@
-{\rtf1\ansi\ansicpg1252\cocoartf2577
-\cocoatextscaling0\cocoaplatform0{\fonttbl\f0\fswiss\fcharset0 Helvetica;}
-{\colortbl;\red255\green255\blue255;}
-{\*\expandedcolortbl;;}
-\margl1440\margr1440\vieww15800\viewh8400\viewkind0
-\pard\tx720\tx1440\tx2160\tx2880\tx3600\tx4320\tx5040\tx5760\tx6480\tx7200\tx7920\tx8640\pardirnatural\partightenfactor0
+pppaddle(int pad_top, pad_bot, pad_col;
+char pad_char;
+};
 
-\f0\fs24 \cf0 #include <stdbool.h>\
-struct pppaddle(int pad_top, pad_bot, pad_col;\
-char pad_char;\
-\};\
-\
-struct pppaddle thePaddle;\
-\
-void paddle_init();\
-void paddle_up();\
-void paddle_down();\
+struct pppaddle thePaddle;
+
+void paddle_init();
+void paddle_up();
+void paddle_down();
 bool paddle_contact(int y, int x);}
