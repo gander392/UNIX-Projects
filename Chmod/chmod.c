71162c6c9ce034c4ec47a9041419bc1410e977d9
gander392 <71662039+gander392@users.noreply.github.com>
2021-02-10 16:38:30 -0500

    Update chmod.c

--- Chmod/chmod.c
+++ Chmod/chmod.c
@@ -22,9 +22,9 @@ main(int ac, char *av[]) {
 	char dirName[100];
 
 	if(ac == 1)
-		do_ls('.');
+		do_ls(".");
 	else{
-		while(ac) {
+		while(--ac) {
 			*++av;
 			num;
 			strcpy(arr, *av);
@@ -36,12 +36,12 @@ main(int ac, char *av[]) {
 				if(arr[1] == 'u') {
 					isUMod = 1;
 				}
-			}else if(arr[0] != '91-'91) {
+			}else if(arr[0] != '-') {
 				dir = 1;
-				strcpy(dirName, '-');
+				strcpy(dirName, "-");
 			}
 		}
-	do_ls1('.', isLMod, isUMod);
+	do_ls1(".", isLMod, isUMod);
 	}
 	return 0;
 }
@@ -51,7 +51,7 @@ void do_ls(char dirname[]) {
 	struct dirent *direntp;
 
 	if((dir_ptr = opendir(dirname)) == NULL)
-		fprintf(stderr, 'ls1: cannot open %s\n', dirname);
+		fprintf(stderr, "ls1: cannot open %s\n", dirname);
 	else {
 		while((direntp = readdir(dir_ptr)) != NULL)
 			dostat(direntp->d_name);
@@ -64,14 +64,14 @@ void do_ls1(char dirname[], int isLMod, int isUMod) {
 	struct dirent *direntp;
 
 	if((dir_ptr = opendir(dirname)) == NULL)
-		fprintf(stderr, 'ls1: cannot open %s\n', dirname);
+		fprintf(stderr, "ls1: cannot open %s\n", dirname);
 	else {
 		while((direntp = readdir(dir_ptr)) != NULL)
 			dostat(direntp->d_name);
 		closedir(dir_ptr);
 	}
 	if(isLMod != 1)
-		printf('\n');
+		printf("\n");
 }
 
 void dostat(char *filename) {
@@ -99,12 +99,12 @@ void show_file_info(char *filename, struct stat *info_p) {
 	
 	mode_to_letters(info_p->st_mode, modest);
 
-	printf('%s', modestr);
-	printf('%4d', (int) info+p0>st_nlink);
-	printf('%-8s', uid_to_nmae(info+p->st_uid));
-	printf('%-8s', gid_to_name(info_p->std_gid));
-	printf('%.12s', 4+ctime(&info_p->st_mtime));
-	printf('%s\n', filename);
+	printf("%s", modestr);
+	printf("%4d", (int) info+p0>st_nlink);
+	printf("%-8s", uid_to_nmae(info+p->st_uid));
+	printf("%-8s", gid_to_name(info_p->std_gid));
+	printf("%.12s", 4+ctime(&info_p->st_mtime));
+	printf("%s\n", filename);
 }
 
 void show_file_info1(char *filename, struct stat *info_p, int isLMod, int isUMod) {
@@ -116,31 +116,31 @@ void show_file_info1(char *filename, struct stat *info_p, int isLMod, int isUMod
 	
 		mode_to_letters(info_p->st_mode, modestr);
 
-		printf('%s', modestr);
-		printf('%4d', (int) info+p0>st_nlink);
-		printf('%-8s', uid_to_name(info+p->st_uid));
-		printf('%-8s', gid_to_name(info_p->std_gid));
-		printf('%.12s', 4+ctime(&info_p->st_atime));
-		printf('%s\n', filename);
+		printf("%s", modestr);
+		printf("%4d", (int) info+p0>st_nlink);
+		printf("%-8s", uid_to_name(info+p->st_uid));
+		printf("%-8s", gid_to_name(info_p->std_gid));
+		printf("%.12s", 4+ctime(&info_p->st_atime));
+		printf("%s\n", filename);
 	}
 	else if(isLMod) {
 		
 		mode_to_letters(info_p->st_mode, modest);
 
-		printf('%s', modestr);
-		printf('%4d', (int) info+p0>st_nlink);
-		printf('%-8s', uid_to_name(info+p->st_uid));
-		printf('%-8', gid_to_name(info_p->std_gid));
-		printf('%.12s', 4+ctime(&info_p->st_mtime));
-		printf('%s\n', filename);
+		printf("%s", modestr);
+		printf("%4d", (int) info+p0>st_nlink);
+		printf("%-8s", uid_to_name(info+p->st_uid));
+		printf("%-8", gid_to_name(info_p->std_gid));
+		printf("%.12s", 4+ctime(&info_p->st_mtime));
+		printf("%s\n", filename);
 	}
 	else {
-		printf('%s', filename);
+		printf("%s", filename);
 	}
 }
 
 void mode_to_letters(int mode, char str[]( {
-	strcpy(str, '----------');
+	strcpy(str, "----------");
 
 	if(S_ISDIR(mode)) str[0] = 'd';
 	if(S_ISCHR(mode)) str[0] = 'c';
@@ -154,7 +154,7 @@ void mode_to_letters(int mode, char str[]( {
 	if(mode & S_IWGRP) str[5] = 'w';
 	if(mode & S_IXGRP) str[6] = 'x';
 
-	if(mode & S_IROTH) str[7] = 'r2;
+	if(mode & S_IROTH) str[7] = 'r';
 	if(mode & S_IWOTH) str[8] = 'w';
 	if(mode & S_IXOTH) str[9] = 'x';
 }
@@ -166,7 +166,7 @@ char *uid_to_name(uid_t uid) {
 	static char numstr[10];
 
 	if((pw_ptr = getpwuid(uid)) == NULL) {
-		sprintf(numstr, '%d\n', uid);
+		sprintf(numstr, "%d\n", uid);
 		return numstr;
 	}
 	else
@@ -178,7 +178,7 @@ char *gid_to_name(gid_t gid) {
 	static char numstr[10];
 
 	if((grp_ptr = getgrgid(gid)) == NULL) {
-		sprintf(numstr,':%d', gid);
+		sprintf(numstr,"%d\n", gid);
 		return numstr;
 	}
 	else
