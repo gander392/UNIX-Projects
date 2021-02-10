31950d0c08bceaf6e7a81a6e771b8d603207b80d
gander392 <71662039+gander392@users.noreply.github.com>
2021-02-09 21:10:51 -0500

    Update pipexexp.c

--- Pipex/pipexexp.c
+++ Pipex/pipexexp.c
@@ -6,13 +6,13 @@
 
 #define oops(m, x) {perror(m), exit(x);}
 
-int childProc = 0; //Determine whether it is a child process relative to main(). Is 0 when it'92s a parent and 1 when it'92s a child
+int childProc = 0; //Determine whether it is a child process relative to main(). Is 0 when it's a parent and 1 when it's a child
 
 void pipeFunc(char **, int, int);
 
 void main(int ac, char **av) {
 	if(ac < 3) {
-		fprintf(stderr, '93usage: ./pipex cmd1 cmd2'85\n\'94);
+		fprintf(stderr, 'usage: ./pipex cmd1 cmd2\n');
 		exit(1);
 	}else
 		pipeFunc(av, 1, 0);
@@ -24,29 +24,29 @@ void pipeFunc(char **av, int i, int read) {
 			if(dup2(read, 0) != -1)
 				close(read); //stdin is successfully redirected
 			else
-				oops('93could not redirect stdin for the last program entered\n'94, 1);
+				oops('could not redirect stdin for the last program entered\n', 1);
 		}
 		execlp(av[i], av[i], NULL);
-		oops('93could not execute the last program entered\n'94, 1);
-	}else\
+		oops('could not execute the last program entered\n', 1);
+	}else
 		int thePipe[2]; //pid
 	
 		if(pipe(thePipe) == -1)
-			oops('93could not pipe\n'94), 1);
+			oops('could not pipe\n'), 1);
 
 		switch(fork()) {
 		case -1:
-			oops('93could not fork\n'94, 1);
+			oops('could not fork\n', 1);
 			break;
 		case 0:
 			childProc = 1;
-			close(thePipe[0]); //brek stdin
+			close(thePipe[0]); //break stdin
 			if(dup2(read, 0) == -1) //read from fdIn
-				oops('93could not redirect stdin\n\'94, 1);
+				oops('could not redirect stdin\n', 1);
 			if(dup2(thePipe[1], 1) == -1) // write to thePipe[1]
-				oops('93could not redirect stdout\n'94, 1);
+				oops('could not redirect stdout\n', 1);
 			execlp(av[i], av[i], NULL);
-			oops('93could not execute\n'94, 1);
+			oops('could not execute\n', 1);
 			break;
 		default:
 			close(thePipe[1]); //parent process executes the remaining programs
