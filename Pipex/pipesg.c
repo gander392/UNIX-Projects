744203148159d3dba489c44329acde547237d38a
gander392 <71662039+gander392@users.noreply.github.com>
2021-02-10 16:20:37 -0500

    Update pipesg.c

--- Pipex/pipesg.c
+++ Pipex/pipesg.c
@@ -9,20 +9,20 @@ main(int ac, char **av) {
 	int thepipe[2], newfd, pid;
 
 	if(ac != 3) {
-		fprintf(stderr, 'usage: pipe cmd1 cmd2\n');
+		fprintf(stderr, "usage: pipe cmd1 cmd2\n");
 		exit(1);
 	
 	if(pipe(the pipe) == -1)
-		oops('cannot get a pipe', 1);
+		oops("cannot get a pipe", 1);
 
 	if((pid = fork()) == -1)
-		oops('cannot fork', 2);
+		oops("cannot fork", 2);
 	//parent will read from pipe
 	if(pid == 0) { //child will execute av[2]
 		close(thepipe[1]; //child doesn't write to pipe
 
 		if(dup2(thepipe[0], 0) == -1)
-			oops('could not redirect stdin', 3);
+			oops("could not redirect stdin", 3);
 
 		close(thepipe[0]); //stdin is duped, close pipe
 		execlp(av[2], av[2], NULL);
@@ -32,7 +32,7 @@ main(int ac, char **av) {
 	close(thepipe[0]); //parent doesn't read from pipe
 	
 	if(dup2(thepipe[1], 1) == -1)
-		oops('could not redirect stdout', 4);
+		oops("could not redirect stdout", 4);
 
 	close(thepipe[1]); //stdout is duped, close pipe
 	execlp(av[1], av[1]. NULL);
